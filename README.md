7564348d703b20f203248141c7534559e74ded1b
NicksFed <nick.guzy76@gmail.com>
2023-01-24 16:22:05 -0800

    Add UI logic and handlers
    Co-authored-by: MIke Donovan <Mike@mikedonovan.dev>

--- README.md
+++ README.md
@@ -1,11 +1,83 @@
 ## Tests
 
 ```
-Describe: resgisterAccount
+'registerAccount': [
+  {
+    description: "It instantiates a BankAccount object with 'name' as inputted name and 'balance' as initial deposit",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      return testAccount.name + ' ' + testAccount.balance;
+    },
+    expected: 'Bob Johnson 2300'
+  },
+],
+  
+'BankAccount.prototype.depositFunds': [
+  {
+    description: "It adds the inputted amount to a BankAccount's 'balance' property",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      testAccount.depositFunds(1000);
+      return testAccount.balance;
+    },
+    expected: 3300
+  },
+  {
+    description: "It will not deposit funds if inputted amount is less than zero",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      testAccount.depositFunds(-5);
+      return testAccount.balance;
+    },
+    expected: 2300
+  },
+  {
+    description: "It will not deposit funds if inputted amount is zero",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      testAccount.depositFunds(0);
+      return testAccount.balance;
+    },
+    expected: 2300
+  },
+],
 
-test: "Establish bank account with arguments Name and initial deposit"
-code: 
-  let bobAccount = registerAccount('Bob Johnson', 2300);
-  console.log(bobAccount);
-
-expectedOutput: { name: 'Bob Johnson', balance: 2300 }
+'BankAccount.prototype.withdrawFunds': [
+  {
+    description: "It subtracts the inputted amount from a BankAccount's 'balance' property",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      testAccount.withdrawFunds(1000);
+      return testAccount.balance;
+    },
+    expected: 1300
+  },
+  {
+    description: "It will not withdraw more than the user has in their balance",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      testAccount.withdrawFunds(3000);
+      return testAccount.balance;
+    },
+    expected: 2300
+  },
+  {
+    description: "It will not withdraw funds if inputted amount is less than zero",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      testAccount.withdrawFunds(-5);
+      return testAccount.balance;
+    },
+    expected: 2300
+  },
+  {
+    description: "It will not withdraw funds if inputted amount is zero",
+    code: () => {
+      let testAccount = registerAccount('Bob Johnson', 2300);
+      testAccount.withdrawFunds(0);
+      return testAccount.balance;
+    },
+    expected: 2300
+  },
+],
+```
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,97 @@
+//Business logic
 function BankAccount(name, initialDeposit) {
   this.name = name;
   this.balance = initialDeposit;
 }
 
-BankAccount.prototype.depositFunds = function(amount) {
-  if (amount > 0){ 
+BankAccount.prototype.depositFunds = function (amount) {
+  if (amount > 0) {
     this.balance += amount;
+  } else {
+    displayErrorMessage("INVALID DEPOSIT AMOUNT", 1200);
   }
 }
-BankAccount.prototype.withdrawFunds = function(amount) {
+BankAccount.prototype.withdrawFunds = function (amount) {
   let sufficient = this.balance >= amount;
   let positive = amount > 0;
   if (sufficient && positive) {
     this.balance -= amount;
   } else {
-    
+    let errorMessage = '';
+    if (!sufficient) {
+      errorMessage = 'INSUFFICIENT FUNDS'
+    } else {
+      errorMessage = 'INVALID WITHDRAWAL AMOUNT'
+    }
+    displayErrorMessage(errorMessage, 1200);
   }
-  
+
 }
+
+let currentAccount;
 
 function registerAccount(name, initialDeposit) {
   return new BankAccount(name, initialDeposit);
 }
+
+//User logic
+
+window.addEventListener('load', function () {
+  document.getElementById('register-form').addEventListener('submit', handleRegisterClick);
+  document.getElementById('deposit-withdraw-form').addEventListener('submit', handleTransactionClick);
+  document.getElementById('transaction-select').addEventListener("change", handleTransactionChange)
+});
+
+function handleRegisterClick(e) {
+  e.preventDefault()
+  let name = document.getElementById("name-input").value
+  let balance = parseInt(document.getElementById("balance-input").value)
+  currentAccount = registerAccount(name, balance)
+  document.getElementById("balance-display").innerText = "$" + balance
+  document.getElementById("user-name-display").innerText = name;
+
+  document.getElementById("name-input").value = null;
+  document.getElementById("balance-input").value = null;
+
+  document.querySelector("#register-form > button").disabled = true;
+  document.getElementById("name-input").disabled = true;
+  document.getElementById("balance-input").disabled = true;
+
+  document.querySelector("#deposit-withdraw-form > button").disabled = false;
+  document.getElementById("amount-input").disabled = false;
+  document.getElementById("transaction-select").disabled = false;
+
+  displayErrorMessage("ACCOUNT CREATED!", 1200, true)
+}
+
+function handleTransactionClick(e) {
+  e.preventDefault()
+  let type = document.getElementById("transaction-select").value
+  let amount = parseInt(document.getElementById("amount-input").value)
+  if (type === "deposit") {
+    currentAccount.depositFunds(amount)
+  } else {
+    currentAccount.withdrawFunds(amount)
+  }
+  document.getElementById("balance-display").innerText = "$" + currentAccount.balance
+}
+
+function handleTransactionChange(e) {
+  document.querySelector("#deposit-withdraw-form > button").innerText = e.target.value;
+}
+
+function displayErrorMessage(messageText, duration, green) {
+  let errorMessage = document.getElementById('error-message');
+  errorMessage.innerText = messageText;
+  errorMessage.classList.add('showing');
+  if (green) {
+    errorMessage.classList.add('green-text');
+  } else {
+    errorMessage.classList.remove('green-text');
+  }
+  setTimeout(function () {
+    errorMessage.classList.remove('showing');
+  }, duration);
+}
+
+
